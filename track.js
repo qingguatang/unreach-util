83d5e8ec9316b003d87b9eaace297c2c0821efd0
bencode <bencode@163.com>
2018-09-04 15:29:20 +0800

    bump version to 0.5.0

---
+++ track.js
@@ -0,0 +1,67 @@
+import qs from 'querystring';
+import request from './request';
+import env from './env';
+
+const debug = require('debug')('track');
+
+
+const getLoginUserId = cached(async() => {
+  const data = await request.get('member', 'account/query');
+  return data && data.id;
+});
+
+
+export default async function track({ type, id, data }) {
+  debug('track: [%s] %s', type, id);
+  const url = await getTrackUrl({ type, id, data, ext: '.gif' });
+  const img = new Image();
+  img.src = url;
+}
+
+
+export async function trackSync({ type, id, data }) {
+  debug('track sync: [%s] %s', type, id);
+  const url = await getTrackUrl({ type, id, data });
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+  xhr.send();
+}
+
+
+async function getTrackUrl({ type, id, data, ext = '' }) {
+  type = type || 'click';
+  const userId = await getLoginUserId();
+  if (env === 'development') {
+    type = 'test_' + type;
+  }
+
+  const url = window.location.href.replace(/#.*$/, '');  // remove hash
+  const query = {
+    APIVersion: '0.6.0',  // 保留字段，必选
+    url,
+    __topic__: userId || '',
+    __userAgent__: window.navigator.userAgent,
+    type,
+    id: id || '',
+    data: JSON.stringify(data || {}),
+    _t: Date.now()
+  };
+
+  const trackUrl = 'https://learninqgt.cn-hangzhou.log.aliyuncs.com/logstores/document/track';
+  return `${trackUrl}${ext}?${qs.stringify(query)}`;
+}
+
+
+function cached(fn) {
+  let called = false;
+  let result = null;
+  return function() {
+    if (called) {
+      return result;
+    }
+    called = true;
+    result = fn.apply(this, arguments);
+    return result;
+  };
+}
